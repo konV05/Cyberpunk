f29abb61de269393731b4b3e71348aa6c8d89690
Konst <konzer0day@gmail.com>
2024-09-28 02:17:10 +0300

    html and css ready

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    //Change bg
     const promo = document.querySelector('.promo'),
           promoStyle = getComputedStyle(promo),
           promoBgUrls = ["url('../img/promo-bg/promo-bg-1.jpg')", "url('../img/promo-bg/promo-bg-2.jpg')", "url('../img/promo-bg/promo-bg-3.jpg')"];
@@ -8,4 +9,16 @@ document.addEventListener('DOMContentLoaded', () => {
         promo.style.setProperty('--bg-url', promoBgUrls[bgIndex]);
         bgIndex = (bgIndex === 2) ? 0 : bgIndex+1
     }, 10000);
+
+
+    //Container margin
+    const container = document.querySelector('.container'),
+          sectionBuyGame = document.querySelector('.buy-game');
+    function returnContainerMargin () {
+        sectionBuyGame.style.setProperty('--container-magin', getComputedStyle(container).marginRight);
+    }   
+    window.addEventListener('resize', () => {
+        returnContainerMargin();
+    });
+    returnContainerMargin();
 });
