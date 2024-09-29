d0847c402f18e4e5b47c03b79d12f5d62f8d9b27
Konst <konzer0day@gmail.com>
2024-09-29 22:26:57 +0300

    release 1.0

--- src/js/script.js
+++ src/js/script.js
@@ -21,4 +21,41 @@ document.addEventListener('DOMContentLoaded', () => {
         returnContainerMargin();
     });
     returnContainerMargin();
+
+    //Animations
+    const waitingElem = document.querySelectorAll('[data-animation]');
+
+    function calcOffset(elem) {
+        if (elem.offsetParent == document.querySelector('body')) {
+            return elem.offsetTop;
+        } else {
+            return calcOffset(elem.offsetParent) + elem.offsetTop
+        }
+    }
+
+    function checkTrigger() {
+        const windowOffsetTop = window.innerHeight + window.scrollY;
+        waitingElem.forEach((item) => {
+            const itemOffset = calcOffset(item)
+            if (itemOffset <= windowOffsetTop) {
+                if(item.getAttribute('data-animation-delay') == null) {
+                    item.classList.add(item.getAttribute('data-animation'))
+                }
+                else {
+                    item.style.opacity = '0';
+                    setTimeout(() =>{
+                        item.style.opacity = '';
+                        item.classList.add(item.getAttribute('data-animation'));
+                        item.removeAttribute('data-animation-delay')
+                    }, +item.getAttribute('data-animation-delay'));
+                }
+            }
+        });
+    }
+
+    document.addEventListener('scroll', () =>{
+        checkTrigger()
+    });
+
+    checkTrigger();
 });
